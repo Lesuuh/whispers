b606da6d2770166ba32784eebb534bd28e313fee
Lesuuh <lesuuh01@gmail.com>
2025-10-18 23:05:34 +0100

    some minor ui fixes

--- client/app/_components/modals/ModalContainer.tsx
+++ client/app/_components/modals/ModalContainer.tsx
@@ -12,8 +12,8 @@ export const ModalContainer: React.FC<Modal> = ({
   if (!isOpen) return null;
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 bg-opacity-50"
-      onClick={onClose} // close when clicking outside content
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm"
+      onClick={onClose}
     >
       <div
         className="flex flex-col h-full w-full md:h-auto md:max-h-[80vh] overflow-y-auto md:w-[60%] md:gap-10  relative  md:px-10 px-5  md:py-10 py-5 modal"
